2c1195b44a4628cdbce195b85b3de116330c1b3b
Maximilian <scientificresearch309@gmail.com>
2025-09-26 09:33:44 +0200

    preparing the Site for Vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // import './App.css';
 
+const message = 'welcome to this site!';
+
 function App() {
   return (
     <div className="App">
       <h1>Vitest-Unit-Testing-React-Vite-Typescript</h1>
+      <p>Message: {message}</p>
     </div>
   );
 }
