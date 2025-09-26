f5d16ad299fe3140ab3678c9829f29fdf4323f09
Maximilian <scientificresearch309@gmail.com>
2025-09-26 11:38:43 +0200

    Tests passed with different toBe => [],(),{}

--- src/apptools.test.ts
+++ src/apptools.test.ts
@@ -4,5 +4,7 @@ import { describe, expect, it } from 'vitest';
 describe('test that surround() is correctly implemented', () => {
   it('returns various brackets correctly', () => {
     expect(surround('test', '[]')).toBe('[test]');
+    expect(surround('nnnn', '()')).toBe('(nnnn)');
+    expect(surround('TEST', '{}')).toBe('{TEST}');
   });
 });
