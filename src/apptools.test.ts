e9b5b90d70c9e022992dd8dd58d8aec7287203c8
Maximilian <scientificresearch309@gmail.com>
2025-09-26 11:49:54 +0200

    the second category of TEST are there too

--- src/apptools.test.ts
+++ src/apptools.test.ts
@@ -2,9 +2,16 @@ import { surround } from './apptools';
 import { describe, expect, it } from 'vitest';
 
 describe('test that surround() is correctly implemented', () => {
+  // 1.
   it('returns various brackets correctly', () => {
     expect(surround('test', '[]')).toBe('[test]');
     expect(surround('nnnn', '()')).toBe('(nnnn)');
     expect(surround('TEST', '{}')).toBe('{TEST}');
+  });
+
+  // 2.
+  it('returns double characters correctly', () => {
+    expect(surround('test', '**')).toBe('*test*');
+    expect(surround('bUsBuS', '||')).toBe('|bUsBuS|');
   });
 });
